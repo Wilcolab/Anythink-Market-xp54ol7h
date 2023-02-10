539d294818bb36247805250b00eb87d1b7c450d1
WilcoApp <hello@wilco.gg>
2023-02-10 22:54:48 +0000

    Dataseeding

--- backend/lib/event.js
+++ backend/lib/event.js
@@ -1,24 +1,25 @@
 const axiosLib = require("axios");
 const fs = require("fs");
 
-const WILCO_ID = process.env.WILCO_ID || fs.readFileSync("../.wilco", "utf8");
+const WILCO_ID = process.env.WILCO_ID || fs.readFileSync('../.wilco', 'utf8')
+const baseURL = process.env.ENGINE_BASE_URL || "https://engine.wilco.gg"
 
-const baseURL = "https://engine.wilco.gg";
 const axios = axiosLib.create({
   baseURL: baseURL,
   headers: {
-    "Content-type": "application/json",
+    'Content-type': 'application/json',
   },
 });
 
 async function sendEvent(event, metadata) {
-  const result = await axios.post(
-    `/users/${WILCO_ID}/event`,
-    JSON.stringify({ event, metadata })
-  );
-  return result.data;
+    try {
+        const result = await axios.post(`/users/${WILCO_ID}/event`, JSON.stringify({event, metadata}));
+        return result.data;
+    } catch (error) {
+        console.error(`failed to send event ${event} to Wilco engine`)
+    }
 }
 
 module.exports = {
   sendEvent,
-};
+}
--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,7 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import agent from "../../agent";
 
-const Banner = (props) => {
-  const onSearchChange = (event) => {
-    props.onSearchFilter(
-      event.target.value,
-      (page) => agent.Items.byTitle(event.target.value),
-      agent.Items.byTitle(event.target.value)
-    );
-  };
+const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -17,16 +9,6 @@ const Banner = (props) => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
-          <form>
-            <input
-              style={{ width: "250px" }}
-              type="text"
-              placeholder="what is it that you truly desire"
-              name="term"
-              id="search-box"
-              onChange={onSearchChange}
-            />
-          </form>
           <span> the cool stuff.</span>
         </div>
       </div>
